5be531f86e5becbb92a4d958778bf0941ae1a7e9
AndreaJ2603 <923655210@sfsu.edu>
2026-03-07 12:52:12 -0800

    Speed

--- microproject/js/p5Sketch.js
+++ microproject/js/p5Sketch.js
@@ -1,11 +1,14 @@
 function setup() {
-  createCanvas(400, 400, WEBGL);
+  createCanvas(1200, 400, WEBGL);
 }
 
 function draw() {
   background(200);
+  let moveSpeed = 0;
+  
+  moveSpeed = 0.5 * frameCount;
 
-  drawHexagon3D();
+  drawHexagon3D(moveSpeed);
 
 }
 
@@ -19,20 +22,20 @@ function drawHexagon(cx, cy, cz, r) {
 }
 
 
-function drawHexagon3D() {
+function drawHexagon3D(s) {
   rotateX(3.5);
   rotateY(4);
   
-  let radius = 100;
-  let h = 50; // Height of the prism
+  let radius = 50;
+  let h = 25; // Height of the prism
   
   // Create a 3D Hexagonal Prism
   stroke(0);
   fill(100, 150, 250);
   
   // Top and Bottom faces
-  drawHexagon(0, 0, -h/2, radius);
-  drawHexagon(0, 0, h/2, radius);
+  drawHexagon(s, 0, -h/2, radius);
+  drawHexagon(s, 0, h/2, radius);
   
   // Connecting sides
   beginShape(QUAD_STRIP);
@@ -40,8 +43,8 @@ function drawHexagon3D() {
     let angle = TWO_PI / 6 * i;
     let x = cos(angle) * radius;
     let y = sin(angle) * radius;
-    vertex(x, y, -h/2);
-    vertex(x, y, h/2);
+    vertex(x+s, y, -h/2);
+    vertex(x+s, y, h/2);
   }
   endShape(CLOSE);
 }

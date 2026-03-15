77a9a2ab59a04b5b7ce66e8b8b733e0901e15dcb
AndreaJ2603 <923655210@sfsu.edu>
2026-03-14 22:49:52 -0700

    First text

--- microproject/js/p5Sketch.js
+++ microproject/js/p5Sketch.js
@@ -14,6 +14,7 @@ let isRed = false;
 let beginTime;
 let timeIsUp = false;
 let timeLeft = 60;
+let isStarted = false;
 
 
 
@@ -32,82 +33,92 @@ function draw() {
 
   background(200);
 
+  stroke(255); // White stroke
+  strokeWeight(2);
+
   fill('green');
-  text("Points: " + myPoints,50,60);
-  text("Time Left: " + timeLeft, 850,60);
 
-
-  if (!timeIsUp) {
-    let elapsedTotalTime = int((millis() - beginTime) / 1000);
-    let remainingTime = gameDuration - elapsedTotalTime;
-    timeLeft = remainingTime;
-
-    if (remainingTime <= 0) {
-      remainingTime = 0;
-      timeIsUp = true;
-    }
- 
-  
-
-    fill(myFillColor);
-    stroke(255); // White stroke
-    strokeWeight(2);
-
-    //let currentTime = millis();  
-    let elapsedTime = millis() - startTime;
-    let timeToSwitch = random(1000, 3000);
-    let timeToSwitchBack = 200;
-
-    if (elapsedTime >= timeToSwitch && elapsedTime < timeToSwitch+timeToSwitchBack) {
-      bigRed();
-    } else if (elapsedTime >= timeToSwitch+timeToSwitchBack) {
-      smallBlue();
-      startTime = millis();
-    }
-
-    let moveSpeed = 0;
-  
-  //if (startMoving) {
-      //moveSpeed = 0.005 * frameCount;
-    moveSpeed = moveSpeed + 2;
-  //}
-  
-  /*if (hexX > 700 ) {
-    reverseDirection = true;
-  } 
-  
-  if (hexX < 0) {
-    reverseDirection = false;
-  }
-  
-  if (reverseDirection) {
-    hexX = hexX - moveSpeed;  
+  if (!isStarted) {
+    text("Collect 10 points by clicking when Red.", width/2, height/2);
+    text("(Press any key to start.)", width/2, height/2+60);
   } else {
-    hexX = hexX + moveSpeed;
+    text("Points: " + myPoints,50,60);
+    text("Time Left: " + timeLeft, 850,60);
+
+
+    if (!timeIsUp) {
+      let elapsedTotalTime = int((millis() - beginTime) / 1000);
+      let remainingTime = gameDuration - elapsedTotalTime;
+      timeLeft = remainingTime;
+
+      if (remainingTime <= 0) {
+        remainingTime = 0;
+        timeIsUp = true;
+      }
+  
+    
+
+      fill(myFillColor);
+
+      //let currentTime = millis();  
+      let elapsedTime = millis() - startTime;
+      let timeToSwitch = random(1000, 3000);
+      let timeToSwitchBack = 200;
+
+      if (elapsedTime >= timeToSwitch && elapsedTime < timeToSwitch+timeToSwitchBack) {
+        bigRed();
+      } else if (elapsedTime >= timeToSwitch+timeToSwitchBack) {
+        smallBlue();
+        startTime = millis();
+      }
+
+      let moveSpeed = 0;
+    
+    //if (startMoving) {
+        //moveSpeed = 0.005 * frameCount;
+      moveSpeed = moveSpeed + 2;
+    //}
+    
+    /*if (hexX > 700 ) {
+      reverseDirection = true;
+    } 
+    
+    if (hexX < 0) {
+      reverseDirection = false;
+    }
+    
+    if (reverseDirection) {
+      hexX = hexX - moveSpeed;  
+    } else {
+      hexX = hexX + moveSpeed;
+    }*/
+
+        hexX = hexX + moveSpeed;
+        if (hexX > width ) {
+        hexX = 0;
+      }
+    
+      let hexY = height / 2;
+      let radius = shapeSize
+    
+    push(); 
+    translate(hexX, hexY); 
+    rotate(angle);
+    drawHexagon(radius);
+    pop();
+    angle += 1;
+    
+  /*if (changeShape == 1) {
+    ellipse(mouseX,mouseY,60);
+  } else {
+    square(mouseX, mouseY, 100);
   }*/
 
-      hexX = hexX + moveSpeed;
-      if (hexX > width ) {
-       hexX = 0;
-     }
-  
-    let hexY = height / 2;
-    let radius = shapeSize
-  
-   push(); 
-   translate(hexX, hexY); 
-   rotate(angle);
-   drawHexagon(radius);
-   pop();
-   angle += 1;
-   
-/*if (changeShape == 1) {
-  ellipse(mouseX,mouseY,60);
-} else {
-  square(mouseX, mouseY, 100);
-}*/
+    }
 
   }
+
+  
 
 
 }
@@ -126,6 +137,9 @@ function mousePressed() {
   //changeShape = mousePressedCount%2;
 }
 
+function keyPressed() {
+  isStarted = true;
+}
 
 
 function drawHexagon(radius) {
